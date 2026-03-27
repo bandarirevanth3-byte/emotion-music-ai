f848f969a5c9e12b3f9cf6590c527d4430693c2c
bandarirevanth3-byte <bandarirevanth3@gmail.com>
2026-03-27 21:39:53 +0530

    Refactor music.py for emotion detection and playlists

--- music.py
+++ music.py
@@ -1,103 +1,131 @@
-# ctrl + tilde
-# streamlit run music.py
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
-import cv2 
-import numpy as np 
-import mediapipe as mp 
-from keras.models import load_model
-import webbrowser
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import load_model
+from deepface import DeepFace
+import os
 
-model  = load_model("model.h5")
-label = np.load("labels.npy")
-holistic = mp.solutions.holistic
-hands = mp.solutions.hands
-holis = holistic.Holistic()
-drawing = mp.solutions.drawing_utils
+# ================= CONFIG =================
+st.set_page_config(page_title="AI Emotion Music System", layout="wide")
 
-st.header("Emotion Based Music Recommender")
+st.markdown("<h1 style='text-align:center;'>🎭 AI Emotion Music Recommender</h1>", unsafe_allow_html=True)
 
-if "run" not in st.session_state:
-	st.session_state["run"] = "true"
+# ================= LOAD MODEL =================
+model = load_model("model.h5")
+labels = np.load("labels.npy")
 
-try:
-	emotion = np.load("emotion.npy")[0]
-except:
-	emotion=""
+# ================= MEDIAPIPE =================
+mp_holistic = mp.solutions.holistic
+holis = mp_holistic.Holistic()
 
-if not(emotion):
-	st.session_state["run"] = "true"
-else:
-	st.session_state["run"] = "false"
+# ================= PLAYLIST LINKS =================
+playlist_links = {
+    "Happy": "https://www.youtube.com/embed/videoseries?list=YOUR_HAPPY_ID",
+    "Sad": "https://www.youtube.com/embed/videoseries?list=YOUR_SAD_ID",
+    "Angry": "https://www.youtube.com/embed/videoseries?list=YOUR_ANGRY_ID",
+    "Neutral": "https://www.youtube.com/embed/videoseries?list=YOUR_NEUTRAL_ID"
+}
 
+# ================= SESSION =================
+if "emotion" not in st.session_state:
+    st.session_state["emotion"] = ""
+
+if "person" not in st.session_state:
+    st.session_state["person"] = ""
+
+if "identified" not in st.session_state:
+    st.session_state["identified"] = False
+
+# ================= VIDEO PROCESSOR =================
 class EmotionProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+        frm = cv2.flip(frm, 1)
 
-		##############################
-		frm = cv2.flip(frm, 1)
+        rgb = cv2.cvtColor(frm, cv2.COLOR_BGR2RGB)
+        res = holis.process(rgb)
 
-		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        if res.face_landmarks:
+            lst = []
 
-		lst = []
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
 
-		if res.face_landmarks:
-			for i in res.face_landmarks.landmark:
-				lst.append(i.x - res.face_landmarks.landmark[1].x)
-				lst.append(i.y - res.face_landmarks.landmark[1].y)
+            lst = np.array(lst).reshape(1, -1)
 
-			if res.left_hand_landmarks:
-				for i in res.left_hand_landmarks.landmark:
-					lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-					lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-			else:
-				for i in range(42):
-					lst.append(0.0)
+            # ===== Emotion Detection =====
+            pred = labels[np.argmax(model.predict(lst, verbose=0))]
+            st.session_state["emotion"] = pred
 
-			if res.right_hand_landmarks:
-				for i in res.right_hand_landmarks.landmark:
-					lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-					lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-			else:
-				for i in range(42):
-					lst.append(0.0)
+            # ===== Face Recognition =====
+            if not st.session_state["identified"]:
+                try:
+                    result = DeepFace.find(
+                        img_path=frm,
+                        db_path="database",
+                        enforce_detection=False,
+                        detector_backend="opencv"
+                    )
 
-			lst = np.array(lst).reshape(1,-1)
+                    if len(result[0]) > 0:
+                        name = result[0].iloc[0]['identity'].split(os.sep)[-2]
+                    else:
+                        name = "Unknown"
 
-			pred = label[np.argmax(model.predict(lst))]
+                except:
+                    name = "Unknown"
 
-			print(pred)
-			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+                st.session_state["person"] = name
+                st.session_state["identified"] = True
 
-			np.save("emotion.npy", np.array([pred]))
+            cv2.putText(frm, f"{st.session_state['person']} - {pred}",
+                        (20, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2)
 
-			
-		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
-								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
-								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
-		drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
-		drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+        else:
+            cv2.putText(frm, "No Face Detected",
+                        (20, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 0, 255), 2)
 
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
 
-		##############################
+# ================= UI LAYOUT =================
+col1, col2 = st.columns([1, 1])
 
-		return av.VideoFrame.from_ndarray(frm, format="bgr24")
+# ===== CAMERA =====
+with col1:
+    st.subheader("📷 Live Emotion Detection")
+    webrtc_streamer(
+        key="camera",
+        video_processor_factory=EmotionProcessor
+    )
 
-lang = st.text_input("Language")
-singer = st.text_input("singer")
+# ===== PLAYLIST =====
+with col2:
+    st.subheader("🎵 Your Playlist")
 
-if lang and singer and st.session_state["run"] != "false":
-	webrtc_streamer(key="key", desired_playing_state=True,
-				video_processor_factory=EmotionProcessor)
+    emotion = st.session_state["emotion"]
 
-btn = st.button("Recommend me songs")
+    if emotion in playlist_links:
+        st.markdown(
+            f"""
+            <iframe width="100%" height="300"
+            src="{playlist_links[emotion]}"
+            frameborder="0" allowfullscreen></iframe>
+            """,
+            unsafe_allow_html=True
+        )
+    else:
+        st.info("👉 Detect your emotion to load playlist")
 
-if btn:
-	if not(emotion):
-		st.warning("Please let me capture your emotion first")
-		st.session_state["run"] = "true"
-	else:
-		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
-		np.save("emotion.npy", np.array([""]))
-		st.session_state["run"] = "false"
+# ================= RESULT =================
+st.markdown("---")
+
+if st.session_state["emotion"]:
+    st.success(
+        f"👤 {st.session_state['person']} | 😊 {st.session_state['emotion']}"
+    )
